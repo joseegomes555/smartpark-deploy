450ec714861a71bf2e25d112a6701b17cdae8494
José Gomes <a26428@alunos.ipca.pt>
2025-12-28 01:01:34 +0000

    Enable Swagger in production

--- src/SmartPark.ApiRest/Program.cs
+++ src/SmartPark.ApiRest/Program.cs
@@ -20,11 +20,10 @@ builder.Services.AddDbContext<SmartParkDbContext>(options =>
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 
 app.UseAuthorization();
 app.MapControllers();
